7ebf993e01a6633e15f7657c4deb618fd279e58a
Aurojyoti <developeratbackend@gmail.com>
2023-05-29 15:22:06 +0530

    used spread operator

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -4,11 +4,11 @@ import RestrauntCard from "./RestruantCard";
 const Body = () => {
     return (
         <div className="restraunt-list">
-            <RestrauntCard cloudinaryImageId={restrauntList[0].data.cloudinaryImageId} name={restrauntList[0].data.name}  cuisines={restrauntList[0].data.cuisines} address={restrauntList[0].data.address}/>
-            <RestrauntCard cloudinaryImageId={restrauntList[1].data.cloudinaryImageId} name={restrauntList[1].data.name}  cuisines={restrauntList[1].data.cuisines} address={restrauntList[1].data.address} />
-            <RestrauntCard cloudinaryImageId={restrauntList[2].data.cloudinaryImageId} name={restrauntList[2].data.name}  cuisines={restrauntList[2].data.cuisines} address={restrauntList[2].data.address} />
-            <RestrauntCard cloudinaryImageId={restrauntList[3].data.cloudinaryImageId} name={restrauntList[3].data.name}  cuisines={restrauntList[3].data.cuisines} address={restrauntList[3].data.address} />
-            <RestrauntCard cloudinaryImageId={restrauntList[4].data.cloudinaryImageId} name={restrauntList[4].data.name}  cuisines={restrauntList[4].data.cuisines} address={restrauntList[4].data.address} />
+            <RestrauntCard {...restrauntList[0].data}/>
+            <RestrauntCard {...restrauntList[1].data} />
+            <RestrauntCard {...restrauntList[2].data} />
+            <RestrauntCard {...restrauntList[3].data} />
+            <RestrauntCard {...restrauntList[4].data} />
         </div>
     );
 };
