ee777789711b573ef5da1777cbd6c8144e4da269
Aurojyoti <developeratbackend@gmail.com>
2023-05-29 17:07:53 +0530

    restraunt card updated using map func

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -3,12 +3,11 @@ import RestrauntCard from "./RestruantCard";
 
 const Body = () => {
     return (
-        <div className="restraunt-list">
-            <RestrauntCard {...restrauntList[0].data}/>
-            <RestrauntCard {...restrauntList[1].data} />
-            <RestrauntCard {...restrauntList[2].data} />
-            <RestrauntCard {...restrauntList[3].data} />
-            <RestrauntCard {...restrauntList[4].data} />
+        <div className="restraunt-list">{
+                restrauntList.map((restraunt) => {
+                    return <RestrauntCard {...restraunt.data}/>;
+                })
+            }
         </div>
     );
 };
